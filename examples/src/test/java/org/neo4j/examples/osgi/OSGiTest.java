f7711dc32d3c140a4486c171941f552f2c2aae04
Andreas Büchler <andi.buechler@gmail.com>
2012-11-26 16:32:47 +0100

    Upgrading to 1.6.3 enterprise.

--- examples/src/test/java/org/neo4j/examples/osgi/OSGiTest.java
+++ examples/src/test/java/org/neo4j/examples/osgi/OSGiTest.java
@@ -57,7 +57,7 @@ import org.osgi.framework.Constants;
 
 public class OSGiTest {
 
-    public static final String NEO4J_OSGI_BUNDLE_VERSION = "1.6.1";
+    public static final String NEO4J_OSGI_BUNDLE_VERSION = "1.6.3";
     public static final String GERONIMO_JTA_VERSION = "1.1.1";
 
     @Ignore @Test
